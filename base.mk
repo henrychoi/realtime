09e8b3ebaf41f338002565e1a822ed6fdcc958f1
Henry Choi <henry.omd@gmail.com>
2012-05-03 19:53:29 -0700

    Use BASEDIR variable in Makefile

--- base.mk
+++ base.mk
@@ -1,27 +1,28 @@
 CC := gcc
-THIRDPARTY :=../../ThirdParty
+THIRDPARTY :=$(BASEDIR)/../ThirdParty
 CUNIT :=$(THIRDPARTY)/CUnit-2.1-2/CUnit
 
 LIB :=lib$(TARGET).a
 TEST :=test_$(TARGET)
-_OBJECTS :=$(patsubst %, %.o, $(TARGET) $(LIBSRC))
-CFLAGS := -g $(patsubst %, -I../%, $(NEEDLIBS)) $(APP_CFLAGS)
-LDFLAGS := -Wno-pointer-to-int-cast -I$(CUNIT)/Headers\
+_objects :=$(patsubst %, %.o, $(TARGET) $(LIBSRC))
+CFLAGS := -g $(patsubst %, -I$(BASEDIR)/%, $(NEEDLIBS)) $(APP_CFLAGS)
+LDFLAGS := -Wno-pointer-to-int-cast -Wno-int-to-pointer_cast\
+	-I$(CUNIT)/Headers\
 	-static -L$(CUNIT)/Sources/.libs -lcunit -L. -l$(TARGET)\
-	$(foreach module, $(NEEDLIBS), -L../$(module) -l$(module))\
+	$(foreach module, $(NEEDLIBS), -L$(BASEDIR)/$(module) -l$(module))\
 	$(APP_LDFLAGS)
 
-.PHONY: clean $(NEEDLIBS)
+.PHONY: clean
 all: $(TEST)
 $(TEST): $(TEST).c $(LIB) $(NEEDLIBS)
 	gcc $(CFLAGS) -o $@ $(TEST).c $(LIB) $(LDFLAGS)
-$(LIB): $(_OBJECTS); ar -rs $@ $^
+$(LIB): $(_objects); ar -rs $@ $^
 %.s: %.c; $(CC) -S $(CFLAGS) $^ $(LDFLAGS)
 clean: $(foreach module, $(NEEDLIBS), clean.$(module))
-	-rm -f $(TEST) $(LIB) $(_OBJECTS) *~
+	-rm -f $(TEST) $(LIB) $(_objects) *~
 
-clean.%:;make -C ../$(patsubst clean.%,%, $@) clean
-$(NEEDLIBS):;make -C ../$@
+clean.%:;make -C $(BASEDIR)/$(patsubst clean.%,%, $@) clean
+$(NEEDLIBS):;make -C $(BASEDIR)/$@
 # Appendix ###########################################
 # $@ means the target of the rule
 # $? All prerequisites NEWER than the target, w/ space inbetween
