b7f52b1c0573daa96bfcacf363a10b69c56a6b8e
Henry <henry.omd@gmail.com>
2012-06-19 17:27:00 -0700

    timer works

--- HelloHSM/workspace/take1/src/main.cc
+++ HelloHSM/workspace/take1/src/main.cc
@@ -3,28 +3,32 @@
 #include "xil_cache.h"
 #include "xintc.h"
 #include "xgpio.h"
+#include "xtmrctr.h"
 //#include "xenv_standalone.h"
 
 #define UART_BAUD 9600
 
 static XIntc intc;//This will be initialized in init_platform()
-static XGpio button5;
+
 #define BUTTON5_CHANNEL 1
-//static XTmr timer;
-static u32 button = 0;
+static XGpio button5;
+volatile u32 button = 0;
+volatile bool button_hot = false;
+
+#define MY_TIMER_ID 0
+static XTmrCtr timer;
 
 void PushButtons_ISR(void* p) {
   button = XGpio_DiscreteRead((XGpio*)p, BUTTON5_CHANNEL);
-  //printf("%ld", val);
+  button_hot = true;
   XIntc_Acknowledge(&intc
                   , XPAR_MICROBLAZE_0_INTC_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_INTR);
   XGpio_InterruptClear((XGpio*)p, BUTTON5_CHANNEL);
 }
-void Timer_ISR(void* p) {
+void Timer_ISR(void* p, u8 timerId) {
   print(".");
-  XIntc_Acknowledge(&intc
-                  , XPAR_MICROBLAZE_0_INTC_AXI_TIMER_0_INTERRUPT_INTR);
 }
+
 int init_platform() {
   int status = XST_SUCCESS;
   print("Begin init_platform\n\r");
@@ -50,22 +54,14 @@ int init_platform() {
   // with any channel other than 1. If it is not, this function will assert.
   // After this call, I can, if I wish, poll the GPIO with
   // u32 val = XGpio_DiscreteRead(&button5, BUTTON5_CHANNEL)
-  XGpio_SetDataDirection(&button5, BUTTON5_CHANNEL, 0xFFFFFFFF); //output: 0, input: 1
+  XGpio_SetDataDirection(&button5, BUTTON5_CHANNEL, 0xFFFFFFFF); //out: 0, in: 1
 
   XGpio_InterruptEnable(&button5, 0xFF);
   // Interrupts enabled through XGpio_InterruptEnable() will not be passed
   // through until the global enable bit is set by this function. This
-  // function is designed to allow all interrupts (both channels) to be
+  // function is designed to allow all interrupts to be
   // enabled easily for exiting a critical section.
   XGpio_InterruptGlobalEnable(&button5);
-
-#if 0
-  u32 mask = XGpio_InterruptGetEnabled(&button5);
-  if(mask != 0x1F) {
-    printf("GPIO INT mask %ld != 0x1F", mask);
-    return -mask;
-  }
-#endif
 
   status = XIntc_Connect(&intc
               , XPAR_MICROBLAZE_0_INTC_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_INTR
@@ -76,16 +72,28 @@ int init_platform() {
   }
   XIntc_Enable(&intc, XPAR_MICROBLAZE_0_INTC_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_INTR);
 
-#if 0
+  status = XTmrCtr_Initialize(&timer, XPAR_AXI_TIMER_0_DEVICE_ID);
+  if(status != XST_SUCCESS) {
+    print("XTmrCtr_Initialize failed");
+    return status;
+  }
+  XTmrCtr_SetHandler(&timer, Timer_ISR//called when timer expires
+		  , &timer);
+  XTmrCtr_SetOptions(&timer, MY_TIMER_ID,
+			XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION);
+  XTmrCtr_SetResetValue(&timer, MY_TIMER_ID
+		  , 0xF0000000);//start counting up (default) from this
   status = XIntc_Connect(&intc
               , XPAR_MICROBLAZE_0_INTC_AXI_TIMER_0_INTERRUPT_INTR
-              , Timer_ISR, &timer);
+              , XTmrCtr_InterruptHandler, &timer);
   if(status != XST_SUCCESS) {
     print("XIntc_Connect(AXI_TIMER_0) failed");
     return status;
   }
+  XTmrCtr_Start(&timer, MY_TIMER_ID);//can stop later with XTmrCtr_Stop()
+
   XIntc_Enable(&intc, XPAR_MICROBLAZE_0_INTC_AXI_TIMER_0_INTERRUPT_INTR);
-#endif
+
 #if 0
   status = XIntc_SelfTest(&intc);
   if(status != XST_SUCCESS) {
@@ -103,7 +111,7 @@ int init_platform() {
   print("Finished platform_init\n\r");
   return status;
 }
-void cleanup_platform() {
+void shutdown_platform() {
   Xil_DCacheDisable();
   Xil_ICacheDisable();
 }
@@ -111,13 +119,14 @@ void cleanup_platform() {
 int main() {
   if(init_platform() == XST_SUCCESS) {
     for(;;) {
-      if(button) {
-        printf("%ld", button);
-        button = 0;
+      //Gpio_DiscreteRead(&button5, BUTTON5_CHANNEL);
+      if(button_hot) {
+        printf("%ld\n", button);
+        button_hot = false;
       }
-      asm("nop");
+      //asm("nop");
     }
   }
-  cleanup_platform();
+  shutdown_platform();
   return 0;
 }
