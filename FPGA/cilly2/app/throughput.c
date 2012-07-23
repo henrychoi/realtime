67f4e02c3765d49b6f9b616d80e802296036c00b
Henry Choi <henry.omd@gmail.com>
2012-07-22 19:29:51 -0700

    Flushing PC command to FPGA over asynchronous channel is not working well.

--- FPGA/cilly2/app/throughput.c
+++ FPGA/cilly2/app/throughput.c
@@ -24,7 +24,6 @@ DWORD WINAPI read_thread(LPVOID arg)
       buf += read_bytes, do_bytes -= read_bytes) {
 
       read_bytes = _read(read_fd, buf, do_bytes);
-
       if ((read_bytes < 0) && (errno != EINTR)) {
 	      perror("read() failed");
 	      return 0;
@@ -47,9 +46,9 @@ DWORD WINAPI read_thread(LPVOID arg)
 }
 
 void allwrite(int fd, unsigned char *buf, int len) {
-  int sent = 0;
+  int sent = 0, rc;
   while (sent < len) {
-    int rc = _write(fd, buf + sent, len - sent);
+    rc = _write(fd, buf + sent, len - sent);
     if ((rc < 0) && (errno == EINTR))
       continue;
 
@@ -65,16 +64,26 @@ void allwrite(int fd, unsigned char *buf, int len) {
 
     sent += rc;
   }
+
+  rc = _write(fd, NULL, 0);//flush
+  if(rc) {
+    fprintf(stderr, "Flush failed\n");
+    exit(1);
+  }
 }
 
 int __cdecl main(int argc, char *argv[]) {
-  const char* readfn = "\\\\.\\xillybus_rd", * writefn = "\\\\.\\xillybus_wr";
+  const char *readfn = "\\\\.\\xillybus_rd"
+	  , *loopfn = "\\\\.\\xillybus_rd_loop"
+	  , *writefn = "\\\\.\\xillybus_wr";
+  unsigned char buf[100];
   HANDLE tid[1];
   DWORD startTick, elapsedMS;
   struct xillyfifo fifo;
   struct xillyinfo info;
-  unsigned int fifo_size = 4096*1024, n_frame = 0, cur_frame, n_bytes = 0;
-  int write_fd = _open(writefn, O_WRONLY | _O_BINARY);
+  unsigned int fifo_size = 4096*16, n_frame = 0, cur_frame, n_bytes = 0;
+  int read_bytes;
+  int loop_fd, write_fd = _open(writefn, O_WRONLY | _O_BINARY);
   if (write_fd < 0) {
     if (errno == ENODEV)
       fprintf(stderr, "(Maybe %s a write-only file?)\n", writefn);
@@ -102,6 +111,12 @@ int __cdecl main(int argc, char *argv[]) {
     exit(1);
   }
 
+  loop_fd = _open(loopfn, O_RDONLY | _O_BINARY);
+  if (loop_fd < 0) {
+    perror("Failed to open loopback file");
+    exit(1);
+  }
+
   if (_setmode(1, _O_BINARY) < 0)
     fprintf(stderr, "Failed to set binary mode for standard output\n");
 
@@ -114,6 +129,9 @@ int __cdecl main(int argc, char *argv[]) {
 
   n_frame = atoi(argv[1]);
   allwrite(write_fd, &n_frame, sizeof(n_frame));
+
+  //read_bytes = _read(loop_fd, buf, sizeof(buf));
+
   cur_frame = n_frame;
   startTick = GetTickCount();
   while(n_bytes < (n_frame * 4)) {
@@ -139,6 +157,7 @@ int __cdecl main(int argc, char *argv[]) {
 
   _close(write_fd);
   _close(read_fd);
+  _close(loop_fd);
 
   // Wait for threads to exit
   if (WaitForSingleObject(tid[0], INFINITE) != WAIT_OBJECT_0) 
