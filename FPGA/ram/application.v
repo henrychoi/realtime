682c8fb56830bd8cdebff13b8b01472962f1d9b7
unknown <hchoi@TS-WARROOM-DW7.Nanofluidics.com>
2012-08-09 18:14:51 -0700

    Except for the very first values, the values written to address 0 is coming out in strictly increasing order.

--- FPGA/ram/application.v
+++ FPGA/ram/application.v
@@ -7,102 +7,74 @@ module application#(parameter ADDR_WIDTH=1, APP_DATA_WIDTH=1)
 , input app_rd_data_valid, input[APP_DATA_WIDTH-1:0] app_rd_data
 );
 `include "function.v"
-  localparam ERROR = 0, STARTUP = 1, WRFIFO_WAIT = 2, WR2 = 3, WR_WAIT = 4
-    , JUST_WAIT = 5, RD_WAIT = 6, DATA_WAIT = 7, NUM_STATE = 8;
-  localparam TEST_VAL = {{2{64'h0123456789ABCDEF}}, {2{64'hFEDCBA9876543210}}}
-           , TEST_ADDR = 'h0000000;//{ADDR_WIDTH{1'b0}};
+  localparam WR_WAIT = 0, WR1 = 1, WR2 = 2, RD = 3, NUM_STATE = 4;
+  localparam END_ADDR = 27'h0000000;
   reg[log2(NUM_STATE)-1:0] state;
-  reg[APP_DATA_WIDTH-1:0] data_read;
+  //reg[APP_DATA_WIDTH-1:0] data_read;
   reg bwrite;
-  reg[ADDR_WIDTH-1:0] addr;
   
   assign app_cmd = {2'b00, bwrite};
 
   always @(posedge clk)
     if(reset) begin
       error = `FALSE;
-      data_read <= 0;
-      addr <= 0;//app_addr <= TEST_ADDR;
-      bwrite <= `FALSE;
-      app_en <= `FALSE;
-      app_wdf_data <= 0;//TEST_VAL;
+		app_addr <= 0;
+      bwrite <= `TRUE;
+      app_en <= `TRUE;
+      app_wdf_data <= 0;
       app_wdf_wren <= `FALSE;
       app_wdf_end <= `FALSE;
-      state <= STARTUP;
+      state <= WR_WAIT;
     end else begin
+	   app_wdf_data <= app_wdf_data + `TRUE;
+
       case(state)
-        STARTUP: begin
-          if(app_rdy) begin
+        WR_WAIT: begin
+          if(app_rdy && app_wdf_rdy) begin
             app_en <= `FALSE;
-            app_wdf_wren <= `TRUE;
+            app_wdf_wren <= `TRUE; // Write the first half of the burst
             app_wdf_end <= `FALSE;
-            state <= WRFIFO_WAIT;
+            state <= WR1;
           end
         end
-        WRFIFO_WAIT: begin
-          if(app_wdf_rdy) begin
-            addr <= addr + 'h40;
-            app_wdf_data <= {{(256-APP_DATA_WIDTH){`FALSE}}, addr};
-            app_addr <= addr;
-            bwrite <= `TRUE;
-            app_en <= `TRUE;
-            app_wdf_end <= `TRUE;
-            state <= WR2;
-          end
+        WR1: begin
+			app_wdf_end <= `TRUE;
+			state <= WR2;
         end
         WR2: begin
-          if(app_rdy) app_en <= `FALSE;
-          app_wdf_wren <= `FALSE;
-          app_wdf_end <= `FALSE;
-          state <= WR_WAIT;
+			 app_wdf_wren <= `FALSE;
+			 app_wdf_end <= `FALSE;
+			 if(app_addr == END_ADDR) begin
+			   app_addr <= 0;
+				bwrite <= `FALSE;
+				app_en <= `TRUE;
+			   state <= RD;
+			 end else begin
+			   app_addr <= app_addr + 'h40;
+				bwrite <= `TRUE;
+				app_en <= `TRUE;
+				state <= WR_WAIT;
+			end
         end
-        WR_WAIT: begin
+        RD: begin
           if(app_rdy) begin
-            if(addr == 0/*'h00003C0*/) begin
-              app_en <= `TRUE;
-              bwrite <= `FALSE;
-              //state <= JUST_WAIT;//RD_WAIT;
-              state <= RD_WAIT;
-            end else begin
-              app_en <= `FALSE;
-              app_wdf_wren <= `TRUE;
-              state <= WRFIFO_WAIT;
-            end
-          end
-        end
-        //JUST_WAIT: begin
-        //  wait_timer <= wait_timer + 1'b1;
-        //  if(wait_timer == 0) begin
-        //    app_en = `TRUE;
-        //    state <= RD_WAIT;
-        //  end
-        //end
-        RD_WAIT: begin
-          //app_en <= `TRUE;
-          if(app_rdy) begin
-            app_en <= `FALSE;
-            state <= DATA_WAIT;
-          end
-        end
-        DATA_WAIT: begin
-          if(app_rd_data_valid) begin
-            data_read <= app_rd_data;
-            
-            if(app_rd_data == TEST_VAL) begin
-              bwrite <= `TRUE;
-              app_wdf_wren <= `TRUE;
-              state <= WR_WAIT;
-            end else begin
-              state <= ERROR;
-            end
+				 if(app_addr == END_ADDR) begin
+					app_addr <= 0;
+					bwrite <= `TRUE;
+					app_en <= `TRUE;
+					state <= WR_WAIT;
+				 end else begin
+					app_addr <= app_addr + 'h40;
+					app_en <= `TRUE;
+					state <= RD;
+				end
           end
         end
         default: begin
-          app_en <= `FALSE;
-          bwrite = `TRUE;
+          app_en <= `TRUE;
+          bwrite = `FALSE;
           app_wdf_wren <= `FALSE;
           error = `FALSE;
-          state <= ERROR;
         end
       endcase
     end
