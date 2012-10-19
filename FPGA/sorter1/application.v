7605384ee42e14ca004c29a6fa3f48f7289e983f
Henry <henry.omd@gmail.com>
2012-10-19 07:04:07 -0700

    synchronizer not working; need to step back and rethink the design

--- FPGA/sorter1/application.v
+++ FPGA/sorter1/application.v
@@ -1,26 +1,40 @@
-module application#(parameter DELAY=1, N_PATCH=1, FP_SIZE=1)
+module application#(parameter DELAY=1, SYNC_WINDOW=1, FP_SIZE=1, N_PATCH=1)
 ( input CLK, RESET, output[7:0] GPIO_LED, output ready, patch_ack
 , input patch_val, input[log2(N_PATCH)-1:0] patch_num
 , input[FP_SIZE-1:0] wtsum);
 `include "function.v"
-  localparam N_BRAM = 8, BRAM_ADDR_SIZE = 10, BRAM_DATA_SIZE = 20
-    , BRAM_ADDR_END = {BRAM_ADDR_SIZE{`TRUE}};
-  reg[log2(N_BRAM)-1:0] bram_ptr;
-  reg [BRAM_DATA_SIZE-1:0] din, expected_data;
+  localparam BRAM_ADDR_SIZE = 10, BRAM_DATA_SIZE = FP_SIZE
+    , BRAM_END_ADDR = {BRAM_ADDR_SIZE{`TRUE}}
+    , N_BRAM = 2**(log2(SYNC_WINDOW) - BRAM_ADDR_SIZE);
+  wire[log2(N_BRAM)-1:0] wr_qptr, rd_qptr;
+  reg [BRAM_DATA_SIZE-1:0] din;
   wire[BRAM_DATA_SIZE-1:0] dout[N_BRAM-1:0];
   wire vout[N_BRAM-1:0];
-  reg wr_valid_bit[N_BRAM-1:0], rd_valid_bit[N_BRAM-1:0];
+  reg wr_valid_bit, rd_valid_bit[N_BRAM-1:0];
   
-  reg [BRAM_ADDR_SIZE-1:0] qhead[N_BRAM-1:0]
-                         , qtail[N_BRAM-1:0];
-  wire[BRAM_ADDR_SIZE-1:0] qhead_plus1[N_BRAM-1:0];
-  wire qfull[N_BRAM-1:0], qempty[N_BRAM-1:0];
+  reg [BRAM_ADDR_SIZE-1:0] wr_addr, rd_addr[N_BRAM-1:0];
+  wire[BRAM_ADDR_SIZE-1:0] qhead_plus1;
+  wire[N_BRAM-1:0] qfull, qempty, completed_patch_avail;
   reg  wren[N_BRAM-1:0];
 
   localparam INIT = 0, READY = 1, ERROR = 2, N_STATE = 3;
   reg [log2(N_STATE)-1:0] state;
   assign ready = state == READY;
-  assign patch_ack = `TRUE;
+  
+  reg [log2(N_PATCH)-1:0] wait4patch;//The patch num to be completed
+  wire[log2(N_PATCH)-1:0] wait4patch_plus_sync_window;
+  wire[log2(SYNC_WINDOW):0] offset_from_wait4;
+  //wire[log2(SYNC_WINDOW)-1:0] qpos;
+
+  // Simulate a patch number received over Aurora
+  assign wait4patch_plus_sync_window = wait4patch + SYNC_WINDOW;
+  assign offset_from_wait4 = patch_num[log2(SYNC_WINDOW):0]
+                           - wait4patch[log2(SYNC_WINDOW):0];
+  //assign qpos = offset_from_wait4[0:log2(SYNC_WINDOW)];
+  assign wr_qptr = patch_num[0+:log2(N_BRAM)];
+  assign rd_qptr = wait4patch[0+:log2(N_BRAM)];
+  
+  assign patch_ack = !(|qfull);
 
   reg [23:0] hb_ctr;
   assign GPIO_LED = {7'd0, hb_ctr[23]};
@@ -28,53 +42,79 @@ module application#(parameter DELAY=1, N_PATCH=1, FP_SIZE=1)
   genvar geni;
   generate  
     for(geni=0; geni < N_BRAM; geni=geni+1) begin
-      bram21 bram(.clka(CLK), .wea(wren[geni]), .addra(qhead[geni])
-                , .dina({wr_valid_bit[geni], din})
-                , .clkb(CLK), .addrb(qtail[geni])
+      bram21 bram(.clka(CLK), .wea(wren[geni]), .addra(wr_addr)
+                , .dina({wr_valid_bit, din})
+                , .clkb(CLK), .addrb(rd_addr[geni])
                 , .doutb({vout[geni], dout[geni]})
                 , .sbiterr(), .dbiterr(), .rdaddrecc());
                 
-      assign qhead_plus1[geni] = qhead[geni] + `TRUE;
-      assign qfull[geni] = qhead_plus1[geni] == qtail[geni];
-      assign qempty[geni] = qhead[geni] == qtail[geni];  
+      assign qfull[geni] = qhead_plus1 == rd_addr[geni];
+      assign qempty[geni] = wr_addr == rd_addr[geni];  
+      assign completed_patch_avail[geni] = vout[geni] == rd_valid_bit[geni];
     end
   endgenerate
-    
+  assign qhead_plus1 = wr_addr + `TRUE;
+
   integer i;
-  always @(posedge CLK)
+  always @(posedge CLK) begin
+    din <= #DELAY wtsum;//Delay through register because wren is registered
+
     if(RESET) begin
-      expected_data <= #DELAY 0;
-      bram_ptr <= #DELAY 0; //N_BRAM - `TRUE;
+      wr_valid_bit <= #DELAY `FALSE;
+      wr_addr <= #DELAY 0;
       for(i=0; i < N_BRAM; i=i+1) begin
-        qhead[i] <= #DELAY 0;
-        qtail[i] <= #DELAY 0;
-        wr_valid_bit[i] <= #DELAY `FALSE;
+        rd_addr[i] <= #DELAY 0;
         rd_valid_bit[i] <= #DELAY `TRUE;
         wren[i] <= #DELAY `TRUE;
       end//for
-      din <= #DELAY 0;
+      wait4patch <= #DELAY 0;
       state <= #DELAY INIT;
-      //$display("%d ns: bram_ptr %d, din %d", $time, bram_ptr, din);
+      //$display("%d ns: qptr %d, din %d", $time, qptr, din);
     end else begin
+      if(wr_addr == BRAM_END_ADDR) wr_valid_bit <= #DELAY ~wr_valid_bit;
+
       for(i=0; i < N_BRAM; i=i+1) begin
-        if(qhead[i]==BRAM_ADDR_END)
-          wr_valid_bit[i] <= #DELAY ~wr_valid_bit[i];
-        if(qtail[i]==BRAM_ADDR_END)
+        if(rd_addr[i] == BRAM_END_ADDR)
           rd_valid_bit[i] <= #DELAY ~rd_valid_bit[i];
       end//for
     
       case(state)
         INIT: begin // Write the starting valid bit to BRAM
-          for(i=0; i < N_BRAM; i=i+1) qhead[i] <= #DELAY qhead[i] + `TRUE;
-          if(qhead[0] == BRAM_ADDR_END) begin
+          wr_addr <= #DELAY wr_addr + `TRUE;
+          if(wr_addr == BRAM_END_ADDR) begin
             for(i=0; i < N_BRAM; i=i+1) wren[i] <= #DELAY `FALSE;
             state <= #DELAY READY;
           end
         end
-        READY: begin
+        READY: begin //Process received patch
+          //Q availability check; checking all queues rather than only wr_qptr
+          //will probably make timing easier to meet.  Extra logic cost to
+          //check all queues is neglible.
+          for(i=0; i < N_BRAM; i=i+1) begin
+            if(qfull[i]) begin
+              $display("%d ns: qfull[%d]", $time, i);
+              state <= #DELAY ERROR;
+            end
+          end
+          
+          if(patch_num < wait4patch //Bounds check
+             || patch_num >= wait4patch_plus_sync_window)
+            state <= #DELAY ERROR;
+          else begin
+            for(i=0; i < N_BRAM; i=i+1)
+              wren[i] <= #DELAY patch_val && i == wr_qptr;
+            wr_addr <= #DELAY rd_addr[rd_qptr] //Pick the right Q
+                     + offset_from_wait4[log2(SYNC_WINDOW):log2(N_BRAM)];
+            if(completed_patch_avail[rd_qptr]) begin
+              //Emit and increment wait4patch
+              wait4patch <= #DELAY wait4patch + `TRUE;
+              rd_addr[rd_qptr] <= #DELAY rd_addr[rd_qptr] + `TRUE;
+            end
+          end
         end
         default: begin
         end
       endcase
     end
+  end//always
 endmodule
--- FPGA/sorter1/main.v
+++ FPGA/sorter1/main.v
@@ -15,9 +15,12 @@ module main#(SIMULATION=0)
        , .LOCKED(pll_locked)
        , .PSCLK(`FALSE), .PSEN(`FALSE), .PSINCDEC(`FALSE), .PWRDWN(`FALSE));
 
-  localparam N_PATCH = 600000, FP_SIZE = 20;
+  localparam N_PATCH = 600000 // Total # of patches I expect
+    , SYNC_WINDOW = 2**13//I can handle up to this may out-of-order patches
+    , FP_SIZE = 20;
   reg [log2(N_PATCH)-1:0] random;
   wire[log2(N_PATCH)-1:0] patch_num;
+  reg [FP_SIZE-1:0] ctr;
   wire[FP_SIZE-1:0] wtsum;
   wire app_rdy;
   reg[1:0] ready_r; //To cross the clock domain
@@ -28,30 +31,41 @@ module main#(SIMULATION=0)
     if(SIMULATION)
       aurora_fifo_bram
         fifo(.rst(!pll_locked), .wr_clk(clk_200), .rd_clk(clk_240)
-           , .din({random, random}), .wr_en(fifo_wr), .full(fifo_full)
-           , .rd_en(patch_ack), .dout({patch_num, wtsum}), .empty(fifo_empty)
-           , .valid(fifo_valid), .sbiterr(), .dbiterr());
+           , .din({random, ctr}), .wr_en(fifo_wr), .full(fifo_full)
+           , .rd_en(patch_ack), .dout({patch_num, wtsum})
+           , .empty(fifo_empty), .sbiterr(), .dbiterr());
     else
       aurora_fifo
         fifo(.rst(!pll_locked), .wr_clk(clk_200), .rd_clk(clk_240)
-           , .din({random, random}), .wr_en(fifo_wr), .full(fifo_full)
-           , .rd_en(patch_ack), .dout({patch_num, wtsum}), .empty(fifo_empty)
-           , .sbiterr(), .dbiterr());
+           , .din({random, ctr}), .wr_en(fifo_wr), .full(fifo_full)
+           , .rd_en(patch_ack), .dout({patch_num, wtsum})
+           , .empty(fifo_empty), .sbiterr(), .dbiterr());
   endgenerate
 
-  application#(.DELAY(DELAY), .N_PATCH(N_PATCH), .FP_SIZE(FP_SIZE))
+  application#(.DELAY(DELAY), .SYNC_WINDOW(SYNC_WINDOW), .FP_SIZE(FP_SIZE)
+    , .N_PATCH(N_PATCH))
     app(.CLK(clk_240), .RESET(RESET || !pll_locked)
       , .GPIO_LED(GPIO_LED), .ready(app_rdy), .patch_ack(patch_ack)
       , .patch_val(!fifo_empty), .patch_num(patch_num), .wtsum(wtsum));
 
-  always @(posedge clk_200)// generate pseudo random number for the random
+  always @(posedge clk_200)//Pseudo random number for the patch_num_offset
     if(!pll_locked) begin
+      ctr <= #DELAY 0;
       random <= #DELAY 0; //Seed value for LFSR
       ready_r <= #DELAY 2'b00;
     end else begin
       ready_r[1] <= #DELAY ready_r[0]; //To cross the clock domain
       ready_r[0] <= #DELAY app_rdy;
-      
-      random <= #DELAY ready_r[1] ? {random[8:0], !(random[9]^random[6])} : 0;
-    end    
+
+      if(ready_r[1] && !fifo_full) begin
+        ctr <= #DELAY ctr + `TRUE;
+        random <= #DELAY 
+          {ctr[log2(N_PATCH)-1:log2(SYNC_WINDOW)]
+           // 13 bit (for 8K SYNC_WINDOW) LFSR implementation
+           , random[11:0], !(random[12] ^ random[11] ^ random[10] ^ random[7])};
+      end else begin
+        ctr <= #DELAY 0;
+        random <= #DELAY 0;
+      end
+    end
 endmodule
