9c2f852e71462c24478fdaaddfbda36e0c3330d0
Henry <henry.omd@gmail.com>
2012-11-02 13:24:42 -0700

    Ready to try out compress on ML605 for real.

--- FPGA/cillysync1/app/compress.c
+++ FPGA/cillysync1/app/compress.c
@@ -1,8 +1,11 @@
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <math.h>
+#include <io.h> //for _open, _write, _read
+#include <conio.h>//for _kbhit
 #include "fifo.h"
 
 static int read_fd = 0;
@@ -78,7 +81,7 @@ unsigned char mulaw_compress(float x) {
   float xb = (x - BIAS) * MUxScale;
   if(xb < 0.0f) xb = 0.0f; // bound it
   else if(xb > MU) xb = MU;
-  fresult = CEILING_DIV_LOG1PMU * log(xb + 1.0f);
+  fresult = CEILING_DIV_LOG1PMU * (float)log(xb + 1.0f);
   return (unsigned char)(fresult + 0.5f);//round it
 }
 
@@ -89,9 +92,8 @@ struct pc2fpga {
 };
 
 int __cdecl main(int argc, char *argv[]) {
+#define N_CAM 3
   struct pc2fpga cmd = {0, 0.f};
-
-#ifdef TALK_TO_FPGA
   DWORD startTick, elapsedMS;
   const char* readfn = "\\\\.\\xillybus_rd"
 	  , * writefn = "\\\\.\\xillybus_wr";
@@ -99,101 +101,127 @@ int __cdecl main(int argc, char *argv[]) {
   struct xillyfifo fifo;
   struct xillyinfo info;
   unsigned int fifo_size = 4096*4, cur_frame, n_bytes = 0;
+  int i, write_fd, bTalk2FPGA = (int)(argc > 1), bContinue = 1;
 
-  int write_fd = _open(writefn, O_WRONLY | _O_BINARY);
-  if (write_fd < 0) {
-    if (errno == ENODEV)
-      fprintf(stderr, "(Maybe %s a write-only file?)\n", writefn);
+  if(bTalk2FPGA) {
+    write_fd = _open(writefn, O_WRONLY | _O_BINARY);
+    if (write_fd < 0) {
+      if (errno == ENODEV)
+        fprintf(stderr, "(Maybe %s a write-only file?)\n", writefn);
 
-    fprintf(stderr, "Failed to open %s", writefn);
-    exit(1);
-  }
+      fprintf(stderr, "Failed to open %s", writefn);
+      exit(1);
+    }
 
-  // If more than one FIFO is created, use the total memory needed instead
-  // of fifo_size with SetProcessWorkingSetSize()
-  if ((fifo_size > 20000) &&
-      !SetProcessWorkingSetSize(GetCurrentProcess(),
-				1024*1024 + fifo_size,
-				2048*1024 + fifo_size))
-    errorprint("Failed to enlarge unswappable RAM limit", GetLastError());
+    // If more than one FIFO is created, use the total memory needed instead
+    // of fifo_size with SetProcessWorkingSetSize()
+    if ((fifo_size > 20000) &&
+        !SetProcessWorkingSetSize(GetCurrentProcess(),
+				  1024*1024 + fifo_size,
+				  2048*1024 + fifo_size))
+      errorprint("Failed to enlarge unswappable RAM limit", GetLastError());
 
-  if (fifo_init(&fifo, fifo_size)) {
-    perror("Failed to init");
-    exit(1);
-  }
+    if (fifo_init(&fifo, fifo_size)) {
+      perror("Failed to init");
+      exit(1);
+    }
 
-  read_fd = _open(readfn, O_RDONLY | _O_BINARY);
-  if (read_fd < 0) {
-    perror("Failed to open read file");
-    exit(1);
-  }
+    read_fd = _open(readfn, O_RDONLY | _O_BINARY);
+    if (read_fd < 0) {
+      perror("Failed to open read file");
+      exit(1);
+    }
 
-  if (_setmode(1, _O_BINARY) < 0)
-    fprintf(stderr, "Failed to set binary mode for standard output\n");
+    if (_setmode(1, _O_BINARY) < 0)
+      fprintf(stderr, "Failed to set binary mode for standard output\n");
 
-  // default security, default stack size, default startup flags
-  tid[0] = CreateThread(NULL, 0, read_thread, &fifo, 0, NULL);
-  if (tid[0] == NULL) {
-    errorprint("Failed to create thread", GetLastError());
-    exit(1);
-  }
-#endif
+    // default security, default stack size, default startup flags
+    tid[0] = CreateThread(NULL, 0, read_thread, &fifo, 0, NULL);
+    if (tid[0] == NULL) {
+      errorprint("Failed to create thread", GetLastError());
+      exit(1);
+    }
+  }//end if(bTalk2FPGA)
 
-  for(;;) {
-    unsigned char expected, actual;
-    if(argc > 1) cmd.f = (float)atof(argv[1]);
-    else {
-      printf("Enter a float point number to compress: ");
-      for(; ; Sleep(10)) {
-        if(_kbhit()) {
-          char line[40];
-          if(!gets_s(line, sizeof(line))) { // EOF reached
-              fprintf(stderr, "\nError; please try again.\n");
-          } else {
-            cmd.f = (float)atof(line);
-            break;
+  printf("Usage: [cam select, patch num] [, wtsum]<ENTER>\n"
+    "If just <ENTER>, quit\n"
+    "cam select: bitmap or'ed of 001 (cam0), 010 (cam1), 100 (cam2)\n"
+    "patch num (in hex, start from 0x1). SOF: 0xfffff, EOF: 0xffffe\n"
+    "wtsum: floating point value\n");
+
+  for( ; bContinue; ) {
+    unsigned char expected, actual[N_CAM];
+    unsigned long cams = 0, reply_cams, match, patch_num;
+    for(; ; Sleep(10)) {
+      if(_kbhit()) {
+#define COMMAND_BUFFER_SIZE 80
+        char line[COMMAND_BUFFER_SIZE];
+        if(!gets_s(line, sizeof(line))) { // EOF reached
+            fprintf(stderr, "\nError; please try again.\n");
+        } else {
+          char* comma;
+          cams = strtoul(line, &comma, 2); //0 if no conversion performed
+          if(!cams) { bContinue = 0; goto loop_end; }
+          patch_num = strtoul(comma+1, &comma, 16);
+          if(!patch_num) {
+            fprintf(stderr
+              , "patch num unspecified; see usage above and try again\n");
+            continue;
           }
-        } // end if(_kbhit())
-      } // end for
-    } //end(no cmd line argument)
-    //Calculate the correct answer
+          if(patch_num > 0xFFFFF) patch_num = 0xFFFFF;
+          cmd.u32 = (cams << 24) // cmd nibble is implicitly 0
+            || patch_num;
+          cmd.f = (float)atof(comma+1);
+          break;
+        }
+      } // end if(_kbhit())
+    } // end for
+    
+    expected = mulaw_compress(cmd.f);//The correct answer
 
-    expected = mulaw_compress(cmd.f);
-#ifdef TALK_TO_FPGA
-    allwrite(write_fd, (unsigned char*)&cmd, sizeof(cmd));
-    n_bytes = 0;
-    while(n_bytes < sizeof(unsigned int)) {
-      int do_bytes = fifo_request_drain(&fifo, &info);
-      if (!do_bytes) return 0;
-      if(n_bytes == sizeof(unsigned int)) {
-        printf(" %X", *((unsigned int*)info.addr));
-        break;
-      }
-      fifo_drained(&fifo, do_bytes);//return the buffer
-      n_bytes += do_bytes;
-    } // end while
-#else
-    actual = expected;
-#endif
+    if(bTalk2FPGA) {
+      allwrite(write_fd, (unsigned char*)&cmd, sizeof(cmd));
+      n_bytes = 0;
+      while(n_bytes < sizeof(unsigned int)) {
+        int do_bytes = fifo_request_drain(&fifo, &info);
+        if (!do_bytes) return 0;
+        if(n_bytes == sizeof(unsigned int)) {
+          printf(" %X", *((unsigned int*)info.addr));
+          break;
+        }
+        fifo_drained(&fifo, do_bytes);//return the buffer
+        n_bytes += do_bytes;
+      } // end while
+    } else {
+      reply_cams = cams;
+      for(i=0; i < N_CAM; ++i) actual[i] = expected;
+    }
+    for(match = 0, i=0; i < N_CAM; ++i)
+      match |= (actual[i] == expected) << i;
 
-    if(actual == expected)
-      printf("PASS; %f -> expected %d\n", cmd.f, expected);
-    else
-      printf("FAIL; %f -> expected %d, got %d\n", cmd.f, expected, actual);
+    if(reply_cams == cams && (match & cams) == cams) printf(
+      "PASS; %f compressed to %d\n", cmd.f, expected);
+    else printf(
+      "FAIL; %f compressed to %d: %d != %d: %d, %d, %d\n"
+      , cmd.f, cams, expected, reply_cams, actual[2], actual[1], actual[0]);
+
+loop_end:
+    continue;
   } //end for(;;)
 
   cmd.u32 = CMD_CLOSE;//Make the FPGA close the rd file
-#ifdef TALK_TO_FPGA
-  allwrite(write_fd, (unsigned char*)&cmd, sizeof(cmd));
+  if(bTalk2FPGA) {
+    allwrite(write_fd, (unsigned char*)&cmd, sizeof(cmd));
 
-  _close(write_fd);
-  _close(read_fd);
+    _close(write_fd);
+    _close(read_fd);
 
-  // Wait for threads to exit
-  if (WaitForSingleObject(tid[0], INFINITE) != WAIT_OBJECT_0) 
-    errorprint("Failed waiting for read_thread to terminate", GetLastError());
+    // Wait for threads to exit
+    if (WaitForSingleObject(tid[0], INFINITE) != WAIT_OBJECT_0) 
+      errorprint("Failed waiting for read_thread to terminate"
+      , GetLastError());
 
-  fifo_destroy(&fifo);
-#endif
+    fifo_destroy(&fifo);
+  }
   return 0;
 }
