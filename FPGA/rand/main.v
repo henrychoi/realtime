5a4a8c1dc93aa3eda8dcf9805830a110cf8271ff
Henry <henry.omd@gmail.com>
2012-12-07 13:57:32 -0800

    Need a pseudo random uniform random number generator

---
+++ FPGA/rand/main.v
@@ -0,0 +1,7 @@
+module main#(parameter SIMULATION=0, DELAY=1)
+(input RESET, CLK_P, CLK_N, output[7:0] GPIO_LED);
+`include "function.v"
+  IBUFGDS sysclk_buf(.I(CLK_P), .IB(CLK_N), .O(CLK));
+  application#(.DELAY(DELAY))
+    app(.CLK(CLK), .RESET(RESET), .GPIO_LED(GPIO_LED));
+endmodule
---
+++ FPGA/rand/simmain.v
@@ -0,0 +1,20 @@
+`timescale 100 ps / 100 ps
+module simmain;
+`include "function.v"
+  wire[7:0] GPIO_LED;
+  reg RESET, CLK;
+  wire CLK_N;
+
+  initial begin
+    CLK <= `FALSE;
+    RESET = `FALSE;
+#25 RESET = `TRUE;
+#175 RESET = `FALSE;
+  end
+  
+  assign CLK_N = ~CLK;
+  always #25 CLK <= ~CLK;  
+
+  main#(.SIMULATION(1), .DELAY(2))
+    main(.RESET(RESET), .CLK_P(CLK), .CLK_N(CLK_N), .GPIO_LED(GPIO_LED));
+endmodule
