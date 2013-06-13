c44c6467e567b549d63413c15ea51f72b1cdf7d6
Henry <henry.omd@gmail.com>
2013-06-13 08:32:30 -0700

    Prototype HW counter code on MSP430 Launchpad.

---
+++ MSP430/HWCounter/blink.c
@@ -0,0 +1,23 @@
+#include <msp430g2231.h>
+unsigned long count = 0;
+#pragma vector = TIMERA0_VECTOR
+__interrupt void timerA_ISR(void) {
+	count += 0x10000;
+	if(count & 0x10000) P1OUT ^= 0x40;
+}
+
+int main(void) {
+	WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+	//See slau144i.pdf, section 8.2.6 "Pin Oscillator".  TASSELx = 0 (TACLK)
+	P1SEL = 0x01;//P0.1 is TA0CLK
+	P1DIR = 0x40;//P1.6 is LED2
+
+	//Choices are between MC_1: count up to TACCR0, or MC_2: to 0xFFFF
+	TACTL = (TASSEL_0 | MC_2);//TACLK, DIV1
+
+	TACCTL0 = CCIE; /*Enable timer A0 interrupt*/
+	_enable_interrupts();
+	while(1) {
+		unsigned long fine_count = count + TAR;
+    }
+}
